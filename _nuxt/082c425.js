d5b66cd15a602c06bfc643e53db689d883c0c19c
kazudayo <108916507+kazudayo@users.noreply.github.com>
2024-03-05 14:05:17 +0900

    追加

---
+++ _nuxt/082c425.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{467:function(n,t,e){"use strict";e.r(t);var o={layout:"PagesLayout"},l=e(61),component=Object(l.a)(o,(function(){var n=this;return(0,n._self._c)("div",[n._v("\n  model: "+n._s(this.$route.params.model)+"\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
