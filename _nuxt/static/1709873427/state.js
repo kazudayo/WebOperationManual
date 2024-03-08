3f90d99c607eed5e1192308b65d823531b0f310e
kazudayo <108916507+kazudayo@users.noreply.github.com>
2024-03-08 13:50:45 +0900

    追加

---
+++ _nuxt/static/1709873427/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpages-test\u002F_nuxt\u002Fstatic\u002F1709873427",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fpages-test\u002F",assetsPath:"\u002Fpages-test\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c093f8a7"}}}}(null));
